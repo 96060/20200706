15694c47c20eb70a150a91c669788d3a4342bbc1
96060 <yyuioi070@gmail.com>
2020-07-06 16:59:43 +0900

    error fix try

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var app = express();
+var Util = require('./utils/Util');
+
+app.get('/' , async(req, res)=>{
+    var result = await Util();
+    var resData = JSON.parse(result);
+    console.log(resData);
+    res.send(resData);
+})
+
+
+app.use(express.json);
 
 app.listen(8000, ()=>{
     console.log(`Server is Starting at http://localhost:8000`);
